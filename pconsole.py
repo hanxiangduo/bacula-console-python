7bab662f1110fb9f4aa388008b87b2045e018928
hanxiangduo <hanxiangduo@gmail.com>
2012-09-10 18:14:28 +0800

    add the init file

---
+++ pconsole.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+HELLO = "Hello *UserAgent* calling\n"
+OKHello = "1000 OK:"
+
+PASSWORD = "EgodaEnaYUnocmD+4uggDNOLXhOJiWGc2qwlA3B/su6g"
+
+def cram_md5_respond(dir, password, tls_remote_need, compatible):
+    '''client connect to dir, the dir confiirm the password and the config is correct '''
+    return (tls_remote_need, compatible)
+
+def cram_md5_challenge(dir, password, tls_local_need, compatible):
+    '''client launch the confirm, client confirm the dir is the correct director '''
+    pass
+
+def authenticate_director(dir, confs):
+    '''authenticate the director
+        if the authenticate success return True else False'''
+    pass
